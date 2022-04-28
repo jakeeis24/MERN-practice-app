4633595a0e96c4b6c4c2ead2a6135b39b2f08ad0
Jake Eissler <jakeeissler1@gmail.com>
2022-04-28 09:06:24 -0600

    server and test route created. server is running on 5000

---
+++ mern-app/backend/server.js
@@ -0,0 +1,15 @@
+import express from "express";
+import cors from "cors";
+import restaurants from "./api/restaurants.route.js";
+
+const app = express();
+
+//middleware
+app.use(cors());
+app.unsubscribe(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+app.use("/api/v1/restaurants", restaurants);
+app.use("*", (req, res) => res.status(404).json({ error: "not found" }));
+
+export default app;
